3d4d3f209cf3ad3aa350c8f5c669dcaef94e36f2
robertbudzynski <rbudzyk@gmail.com>
2018-12-29 18:35:21 +0000

    allowed host added

--- city_app/settings.py
+++ city_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['robertbudzynski.pythonanywhere.com']
 
 
 # Application definition
